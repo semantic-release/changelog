5755ce74efa168a68955aad3775170e54d0c74b4
Pierre Vanduynslager <pierre.denis.vanduynslager@gmail.com>
2018-02-11 17:53:53 -0500

    feat: return all errors
    Allow to report to the user multiple config/auth errors at once

--- index.js
+++ index.js
@@ -7,12 +7,10 @@ let verified;
 async function verifyConditions(pluginConfig, {options}) {
   // If the Changelog publish plugin is used and has `changelogFile` configured, validate them now in order to prevent any release if the configuration is wrong
   if (options.publish) {
-    const publishPlugin = castArray(options.publish).find(
-      config => config.path && config.path === '@semantic-release/changelog'
-    );
-    if (publishPlugin && publishPlugin.changelogFile) {
-      pluginConfig.changelogFile = publishPlugin.changelogFile;
-    }
+    const publishPlugin =
+      castArray(options.publish).find(config => config.path && config.path === '@semantic-release/changelog') || {};
+
+    pluginConfig.changelogFile = pluginConfig.changelogFile || publishPlugin.changelogFile;
   }
   await verifyChangelog(pluginConfig);
   verified = true;
