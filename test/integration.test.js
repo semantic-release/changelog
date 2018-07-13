89de5294b44030ec8a5d0ef9173bfca24fef105f
Pierre Vanduynslager <pierre.denis.vanduynslager@gmail.com>
2018-07-13 00:43:44 -0400

    feat: use `cwd` and `env` options passed by core
    feat: use `cwd` and `env` options passed by core
    
    BREAKING CHANGE: require `semantic-release` >= `15.8.0`

--- test/integration.test.js
+++ test/integration.test.js
@@ -1,15 +1,11 @@
+import path from 'path';
 import test from 'ava';
 import {outputFile, readFile} from 'fs-extra';
 import {stub} from 'sinon';
 import clearModule from 'clear-module';
 import tempy from 'tempy';
 
-// Save the current working diretory
-const cwd = process.cwd();
-
 test.beforeEach(t => {
-  // Change current working directory to a temp directory
-  process.chdir(tempy.directory());
   // Clear npm cache to refresh the module state
   clearModule('..');
   t.context.m = require('..');
@@ -18,62 +14,66 @@ test.beforeEach(t => {
   t.context.logger = {log: t.context.log};
 });
 
-test.afterEach.always(() => {
-  // Restore the current working directory
-  process.chdir(cwd);
-});
-
 test.serial('Verify "changelogFile"', async t => {
-  const changelogFile = 'docs/changelog.txt';
+  const cwd = tempy.directory();
   const notes = 'Test release note';
+  const changelogFile = 'docs/changelog.txt';
 
-  await t.notThrows(t.context.m.verifyConditions({changelogFile}, {nextRelease: {notes}, options: {}}));
+  await t.notThrows(t.context.m.verifyConditions({changelogFile}, {cwd, options: {}, nextRelease: {notes}}));
 });
 
 test.serial('Create new CHANGELOG.md', async t => {
-  const changelogFile = 'docs/changelog.txt';
+  const cwd = tempy.directory();
   const notes = 'Test release note';
+  const changelogFile = 'docs/changelog.txt';
+  const changelogPath = path.resolve(cwd, changelogFile);
 
-  await t.context.m.prepare({changelogFile}, {nextRelease: {notes}, logger: t.context.logger});
+  await t.context.m.prepare({changelogFile}, {cwd, options: {}, nextRelease: {notes}, logger: t.context.logger});
 
   // Verify the content of the CHANGELOG.md
-  t.is((await readFile(changelogFile)).toString(), `${notes}\n`);
+  t.is((await readFile(changelogPath)).toString(), `${notes}\n`);
 
-  t.deepEqual(t.context.log.args[0], ['Create %s', changelogFile]);
+  t.deepEqual(t.context.log.args[0], ['Create %s', changelogPath]);
 });
 
 test.serial('Skip changelog update if the release is empty', async t => {
-  await outputFile('CHANGELOG.md', 'Initial CHANGELOG');
+  const cwd = tempy.directory();
+  const changelogFile = 'CHANGELOG.txt';
+  const changelogPath = path.resolve(cwd, changelogFile);
+  await outputFile(changelogPath, 'Initial CHANGELOG');
 
-  await t.context.m.prepare({}, {nextRelease: {}, options: {}, logger: t.context.logger});
+  await t.context.m.prepare({}, {cwd, options: {}, nextRelease: {}, logger: t.context.logger});
 
   // Verify the content of the CHANGELOG.md
-  t.is((await readFile('CHANGELOG.md')).toString(), 'Initial CHANGELOG');
+  t.is((await readFile(changelogPath)).toString(), 'Initial CHANGELOG');
 });
 
 test.serial('Verify only on the fist call', async t => {
-  const changelogFile = 'docs/changelog.txt';
+  const cwd = tempy.directory();
   const notes = 'Test release note';
+  const changelogFile = 'docs/changelog.txt';
+  const changelogPath = path.resolve(cwd, changelogFile);
 
   await t.context.m.verifyConditions(
     {changelogFile},
     {nextRelease: {notes}, options: {prepare: ['@semantic-release/git']}}
   );
-  await t.context.m.prepare({changelogFile}, {nextRelease: {notes}, logger: t.context.logger});
+  await t.context.m.prepare({changelogFile}, {cwd, nextRelease: {notes}, logger: t.context.logger});
 
   // Verify the content of the CHANGELOG.md
-  t.is((await readFile(changelogFile)).toString(), `${notes}\n`);
+  t.is((await readFile(changelogPath)).toString(), `${notes}\n`);
 
-  t.deepEqual(t.context.log.args[0], ['Create %s', changelogFile]);
+  t.deepEqual(t.context.log.args[0], ['Create %s', changelogPath]);
 });
 
 test('Throw SemanticReleaseError if prepare "changelogFile" option is not a string', async t => {
+  const cwd = tempy.directory();
   const changelogFile = 42;
   const errors = [
     ...(await t.throws(
       t.context.m.verifyConditions(
         {},
-        {options: {prepare: ['@semantic-release/git', {path: '@semantic-release/changelog', changelogFile}]}}
+        {cwd, options: {prepare: ['@semantic-release/git', {path: '@semantic-release/changelog', changelogFile}]}}
       )
     )),
   ];
