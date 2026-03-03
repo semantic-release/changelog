b9a5e770fa74d8fbdd6fc5e0b27ede714d20d3bf
babblebey <babblebey@gmail.com>
2026-03-03 18:04:06 +0100

    refactor: migrate index.js to ES module syntax

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
 /* eslint require-atomic-updates: off */
 
-const {defaultTo, castArray} = require('lodash');
-const verifyChangelog = require('./lib/verify.js');
-const prepareChangelog = require('./lib/prepare.js');
+import {defaultTo, castArray} from 'lodash-es';
+import verifyChangelog from './lib/verify.js';
+import prepareChangelog from './lib/prepare.js';
 
-let verified;
+export default function changelogPlugin() {
+  let verified;
 
-async function verifyConditions(pluginConfig, context) {
-  const {options} = context;
-  // If the Changelog prepare plugin is used and has `changelogFile` configured, validate them now in order to prevent any release if the configuration is wrong
-  if (options.prepare) {
-    const preparePlugin =
-      castArray(options.prepare).find((config) => config.path && config.path === '@semantic-release/changelog') || {};
+  async function verifyConditions(pluginConfig, context) {
+    const {options} = context;
+    // If the Changelog prepare plugin is used and has `changelogFile` configured, validate them now in order to prevent any release if the configuration is wrong
+    if (options.prepare) {
+      const preparePlugin =
+        castArray(options.prepare).find((config) => config.path && config.path === '@semantic-release/changelog') || {};
 
-    pluginConfig.changelogFile = defaultTo(pluginConfig.changelogFile, preparePlugin.changelogFile);
-  }
+      pluginConfig.changelogFile = defaultTo(pluginConfig.changelogFile, preparePlugin.changelogFile);
+    }
 
-  await verifyChangelog(pluginConfig);
-  verified = true;
-}
-
-async function prepare(pluginConfig, context) {
-  if (!verified) {
     await verifyChangelog(pluginConfig);
     verified = true;
   }
 
-  await prepareChangelog(pluginConfig, context);
+  async function prepare(pluginConfig, context) {
+    if (!verified) {
+      await verifyChangelog(pluginConfig);
+      verified = true;
+    }
+
+    await prepareChangelog(pluginConfig, context);
+  }
+
+  return {verifyConditions, prepare};
 }
 
-module.exports = {verifyConditions, prepare};
+const {verifyConditions, prepare} = changelogPlugin();
+export {verifyConditions, prepare};
